945575c9a4b5469f440ee6ef8e11e3b95d5c6baa
Dmitry Purtov <dmtr.pv@gmail.com>
2021-01-25 06:19:53 +0300

    Add XCode project

---
+++ source/ContestKit-iOS/ContestKit_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContestKit_iOSApp.swift
+//  ContestKit-iOS
+//
+//  Created by Dmitry Purtov on 25.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContestKit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
