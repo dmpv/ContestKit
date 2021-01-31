fa906044065d6d19dc108f406f80ab257dd75ea6
Dmitry Purtov <dmtr.pv@gmail.com>
2021-01-31 14:53:56 +0300

    Remove SwiftUI and scenes, use AppDelegate

---
+++ source/modules/ContestKit-iOS/ContestKit-iOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  ContestKit-iOS
+//
+//  Created by Dmitry Purtov on 31.01.2021.
+//
+
+import UIKit
+
+import ContestKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow()
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+
+        AppComponents.shared.store.dispatch(
+            AppComponents.shared.module.startEditing()
+        )
+
+        return true
+    }
+}
